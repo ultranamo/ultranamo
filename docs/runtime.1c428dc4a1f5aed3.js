f5c51096de9ca005de90a93214103d6fe4a96718
Phongsak thomthitchong <senchabox@gmail.com>
2022-12-20 00:54:38 +0700

    delete all www

--- docs/runtime.1c428dc4a1f5aed3.js
+++ docs/runtime.1c428dc4a1f5aed3.js
@@ -1,1 +1,260 @@
-(()=>{"use strict";var e,v={},g={};function t(e){var f=g[e];if(void 0!==f)return f.exports;var a=g[e]={exports:{}};return v[e].call(a.exports,a,a.exports,t),a.exports}t.m=v,e=[],t.O=(f,a,d,n)=>{if(!a){var r=1/0;for(c=0;c<e.length;c++){for(var[a,d,n]=e[c],l=!0,o=0;o<a.length;o++)(!1&n||r>=n)&&Object.keys(t.O).every(p=>t.O[p](a[o]))?a.splice(o--,1):(l=!1,n<r&&(r=n));if(l){e.splice(c--,1);var b=d();void 0!==b&&(f=b)}}return f}n=n||0;for(var c=e.length;c>0&&e[c-1][2]>n;c--)e[c]=e[c-1];e[c]=[a,d,n]},t.n=e=>{var f=e&&e.__esModule?()=>e.default:()=>e;return t.d(f,{a:f}),f},(()=>{var f,e=Object.getPrototypeOf?a=>Object.getPrototypeOf(a):a=>a.__proto__;t.t=function(a,d){if(1&d&&(a=this(a)),8&d||"object"==typeof a&&a&&(4&d&&a.__esModule||16&d&&"function"==typeof a.then))return a;var n=Object.create(null);t.r(n);var c={};f=f||[null,e({}),e([]),e(e)];for(var r=2&d&&a;"object"==typeof r&&!~f.indexOf(r);r=e(r))Object.getOwnPropertyNames(r).forEach(l=>c[l]=()=>a[l]);return c.default=()=>a,t.d(n,c),n}})(),t.d=(e,f)=>{for(var a in f)t.o(f,a)&&!t.o(e,a)&&Object.defineProperty(e,a,{enumerable:!0,get:f[a]})},t.f={},t.e=e=>Promise.all(Object.keys(t.f).reduce((f,a)=>(t.f[a](e,f),f),[])),t.u=e=>(({2214:"polyfills-core-js",6748:"polyfills-dom",8592:"common"}[e]||e)+"."+{388:"d23aef83944a1665",438:"3aa1b2330d912846",657:"5eb0b715428bef72",1033:"f35decb9160b9181",1118:"67ae67b707fd7777",1186:"4e866a433f039c53",1217:"260c594c93a5c6ce",1536:"e29e723c1731bb02",1650:"0c5003aa78ea82a5",1709:"499223cd2845af81",2073:"5982c93672301d9b",2175:"3cf8b000f71ff0c7",2214:"b3eb16f0f64e9b8c",2289:"78258bcc52377c92",2349:"edf956f07a2aa5f3",2698:"091c6952271181ff",2773:"fb4633a741b876e8",3236:"38e6c182f4f373b3",3269:"b43e4f7acacf13ab",3648:"1f96a56ab590d36c",3804:"d5a4d3f2fdb0127d",4174:"a2a5b18bafbee75c",4330:"ac2a5d1b8b7bb471",4376:"ab6853f1abc90def",4432:"f62ec9b6c1a40a2d",4651:"2e400592e20cfdd2",4711:"90bff0e8d0cc0264",4753:"6928fc5c16fcef78",4851:"44f92b8ab1e6011a",4908:"5bdf640b07b0f929",4959:"e0e40bebd75317a5",5168:"0a898cb999fc3e2d",5349:"f7c3d88736084982",5652:"1fa6b36a0591ff7c",5780:"746963ab8004ee58",5817:"a096ab3ab0722d3e",5836:"615fa70e6f185721",6120:"3d3e2443e5ecc6f8",6560:"6bfb9f331a31ec02",6748:"3a5e3168052f1fc5",6868:"b750538ac74e5424",7544:"3e1a473106960b3a",7602:"c0e0ffe4b5a279c5",8136:"6ade51f950957faa",8592:"7dfe90f75d8806f3",8628:"07b13ffcebcc92e0",8939:"e268846754d2f8fb",9016:"c9db6e7c0f38d6ae",9230:"4115309597bd4455",9325:"dc13a3a1a3f1d845",9434:"c75b61fe30c4ffcf",9536:"52e495f7146f8579",9654:"a35522ec373b012a",9718:"735f7870bf946271",9824:"72592d7279d164c7",9922:"07fd84d891e0141e",9958:"3fb43ed463fe4883"}[e]+".js"),t.miniCssF=e=>{},t.o=(e,f)=>Object.prototype.hasOwnProperty.call(e,f),(()=>{var e={},f="app:";t.l=(a,d,n,c)=>{if(e[a])e[a].push(d);else{var r,l;if(void 0!==n)for(var o=document.getElementsByTagName("script"),b=0;b<o.length;b++){var i=o[b];if(i.getAttribute("src")==a||i.getAttribute("data-webpack")==f+n){r=i;break}}r||(l=!0,(r=document.createElement("script")).type="module",r.charset="utf-8",r.timeout=120,t.nc&&r.setAttribute("nonce",t.nc),r.setAttribute("data-webpack",f+n),r.src=t.tu(a)),e[a]=[d];var s=(m,p)=>{r.onerror=r.onload=null,clearTimeout(u);var y=e[a];if(delete e[a],r.parentNode&&r.parentNode.removeChild(r),y&&y.forEach(_=>_(p)),m)return m(p)},u=setTimeout(s.bind(null,void 0,{type:"timeout",target:r}),12e4);r.onerror=s.bind(null,r.onerror),r.onload=s.bind(null,r.onload),l&&document.head.appendChild(r)}}})(),t.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},(()=>{var e;t.tt=()=>(void 0===e&&(e={createScriptURL:f=>f},"undefined"!=typeof trustedTypes&&trustedTypes.createPolicy&&(e=trustedTypes.createPolicy("angular#bundler",e))),e)})(),t.tu=e=>t.tt().createScriptURL(e),t.p="",(()=>{var e={3666:0};t.f.j=(d,n)=>{var c=t.o(e,d)?e[d]:void 0;if(0!==c)if(c)n.push(c[2]);else if(3666!=d){var r=new Promise((i,s)=>c=e[d]=[i,s]);n.push(c[2]=r);var l=t.p+t.u(d),o=new Error;t.l(l,i=>{if(t.o(e,d)&&(0!==(c=e[d])&&(e[d]=void 0),c)){var s=i&&("load"===i.type?"missing":i.type),u=i&&i.target&&i.target.src;o.message="Loading chunk "+d+" failed.\n("+s+": "+u+")",o.name="ChunkLoadError",o.type=s,o.request=u,c[1](o)}},"chunk-"+d,d)}else e[d]=0},t.O.j=d=>0===e[d];var f=(d,n)=>{var o,b,[c,r,l]=n,i=0;if(c.some(u=>0!==e[u])){for(o in r)t.o(r,o)&&(t.m[o]=r[o]);if(l)var s=l(t)}for(d&&d(n);i<c.length;i++)t.o(e,b=c[i])&&e[b]&&e[b][0](),e[b]=0;return t.O(s)},a=self.webpackChunkapp=self.webpackChunkapp||[];a.forEach(f.bind(null,0)),a.push=f.bind(null,a.push.bind(a))})()})();
+(() => {
+  "use strict";
+  var e,
+    v = {},
+    g = {};
+  function t(e) {
+    var f = g[e];
+    if (void 0 !== f) return f.exports;
+    var a = (g[e] = { exports: {} });
+    return v[e].call(a.exports, a, a.exports, t), a.exports;
+  }
+  (t.m = v),
+    (e = []),
+    (t.O = (f, a, d, n) => {
+      if (!a) {
+        var r = 1 / 0;
+        for (c = 0; c < e.length; c++) {
+          for (var [a, d, n] = e[c], l = !0, o = 0; o < a.length; o++)
+            (!1 & n || r >= n) && Object.keys(t.O).every((p) => t.O[p](a[o]))
+              ? a.splice(o--, 1)
+              : ((l = !1), n < r && (r = n));
+          if (l) {
+            e.splice(c--, 1);
+            var i = d();
+            void 0 !== i && (f = i);
+          }
+        }
+        return f;
+      }
+      n = n || 0;
+      for (var c = e.length; c > 0 && e[c - 1][2] > n; c--) e[c] = e[c - 1];
+      e[c] = [a, d, n];
+    }),
+    (t.n = (e) => {
+      var f = e && e.__esModule ? () => e.default : () => e;
+      return t.d(f, { a: f }), f;
+    }),
+    (() => {
+      var f,
+        e = Object.getPrototypeOf
+          ? (a) => Object.getPrototypeOf(a)
+          : (a) => a.__proto__;
+      t.t = function (a, d) {
+        if (
+          (1 & d && (a = this(a)),
+          8 & d ||
+            ("object" == typeof a &&
+              a &&
+              ((4 & d && a.__esModule) ||
+                (16 & d && "function" == typeof a.then))))
+        )
+          return a;
+        var n = Object.create(null);
+        t.r(n);
+        var c = {};
+        f = f || [null, e({}), e([]), e(e)];
+        for (
+          var r = 2 & d && a;
+          "object" == typeof r && !~f.indexOf(r);
+          r = e(r)
+        )
+          Object.getOwnPropertyNames(r).forEach((l) => (c[l] = () => a[l]));
+        return (c.default = () => a), t.d(n, c), n;
+      };
+    })(),
+    (t.d = (e, f) => {
+      for (var a in f)
+        t.o(f, a) &&
+          !t.o(e, a) &&
+          Object.defineProperty(e, a, { enumerable: !0, get: f[a] });
+    }),
+    (t.f = {}),
+    (t.e = (e) =>
+      Promise.all(Object.keys(t.f).reduce((f, a) => (t.f[a](e, f), f), []))),
+    (t.u = (e) =>
+      (({ 2214: "polyfills-core-js", 6748: "polyfills-dom", 8592: "common" }[
+        e
+      ] || e) +
+      "." +
+      {
+        388: "d23aef83944a1665",
+        438: "3aa1b2330d912846",
+        657: "5eb0b715428bef72",
+        1033: "f35decb9160b9181",
+        1118: "67ae67b707fd7777",
+        1186: "4e866a433f039c53",
+        1217: "260c594c93a5c6ce",
+        1536: "e29e723c1731bb02",
+        1650: "0c5003aa78ea82a5",
+        1709: "499223cd2845af81",
+        2073: "5982c93672301d9b",
+        2175: "3cf8b000f71ff0c7",
+        2214: "b3eb16f0f64e9b8c",
+        2289: "78258bcc52377c92",
+        2349: "edf956f07a2aa5f3",
+        2698: "091c6952271181ff",
+        2773: "fb4633a741b876e8",
+        3236: "38e6c182f4f373b3",
+        3269: "b43e4f7acacf13ab",
+        3648: "1f96a56ab590d36c",
+        3804: "d5a4d3f2fdb0127d",
+        4174: "a2a5b18bafbee75c",
+        4330: "ac2a5d1b8b7bb471",
+        4376: "ab6853f1abc90def",
+        4432: "f62ec9b6c1a40a2d",
+        4651: "2e400592e20cfdd2",
+        4711: "90bff0e8d0cc0264",
+        4753: "6928fc5c16fcef78",
+        4851: "69d8a7ddeb4d9e63",
+        4908: "5bdf640b07b0f929",
+        4959: "e0e40bebd75317a5",
+        5168: "0a898cb999fc3e2d",
+        5349: "f7c3d88736084982",
+        5652: "1fa6b36a0591ff7c",
+        5780: "746963ab8004ee58",
+        5817: "a096ab3ab0722d3e",
+        5836: "615fa70e6f185721",
+        6120: "3d3e2443e5ecc6f8",
+        6560: "6bfb9f331a31ec02",
+        6748: "3a5e3168052f1fc5",
+        6868: "b750538ac74e5424",
+        7544: "3e1a473106960b3a",
+        7602: "c0e0ffe4b5a279c5",
+        8136: "6ade51f950957faa",
+        8592: "7dfe90f75d8806f3",
+        8628: "07b13ffcebcc92e0",
+        8939: "e268846754d2f8fb",
+        9016: "c9db6e7c0f38d6ae",
+        9230: "4115309597bd4455",
+        9325: "dc13a3a1a3f1d845",
+        9434: "c75b61fe30c4ffcf",
+        9536: "52e495f7146f8579",
+        9654: "a35522ec373b012a",
+        9718: "735f7870bf946271",
+        9824: "72592d7279d164c7",
+        9922: "07fd84d891e0141e",
+        9958: "3fb43ed463fe4883",
+      }[e] +
+      ".js")),
+    (t.miniCssF = (e) => {}),
+    (t.o = (e, f) => Object.prototype.hasOwnProperty.call(e, f)),
+    (() => {
+      var e = {},
+        f = "app:";
+      t.l = (a, d, n, c) => {
+        if (e[a]) e[a].push(d);
+        else {
+          var r, l;
+          if (void 0 !== n)
+            for (
+              var o = document.getElementsByTagName("script"), i = 0;
+              i < o.length;
+              i++
+            ) {
+              var b = o[i];
+              if (
+                b.getAttribute("src") == a ||
+                b.getAttribute("data-webpack") == f + n
+              ) {
+                r = b;
+                break;
+              }
+            }
+          r ||
+            ((l = !0),
+            ((r = document.createElement("script")).type = "module"),
+            (r.charset = "utf-8"),
+            (r.timeout = 120),
+            t.nc && r.setAttribute("nonce", t.nc),
+            r.setAttribute("data-webpack", f + n),
+            (r.src = t.tu(a))),
+            (e[a] = [d]);
+          var s = (m, p) => {
+              (r.onerror = r.onload = null), clearTimeout(u);
+              var y = e[a];
+              if (
+                (delete e[a],
+                r.parentNode && r.parentNode.removeChild(r),
+                y && y.forEach((_) => _(p)),
+                m)
+              )
+                return m(p);
+            },
+            u = setTimeout(
+              s.bind(null, void 0, { type: "timeout", target: r }),
+              12e4
+            );
+          (r.onerror = s.bind(null, r.onerror)),
+            (r.onload = s.bind(null, r.onload)),
+            l && document.head.appendChild(r);
+        }
+      };
+    })(),
+    (t.r = (e) => {
+      "undefined" != typeof Symbol &&
+        Symbol.toStringTag &&
+        Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }),
+        Object.defineProperty(e, "__esModule", { value: !0 });
+    }),
+    (() => {
+      var e;
+      t.tt = () => (
+        void 0 === e &&
+          ((e = { createScriptURL: (f) => f }),
+          "undefined" != typeof trustedTypes &&
+            trustedTypes.createPolicy &&
+            (e = trustedTypes.createPolicy("angular#bundler", e))),
+        e
+      );
+    })(),
+    (t.tu = (e) => t.tt().createScriptURL(e)),
+    (t.p = ""),
+    (() => {
+      var e = { 3666: 0 };
+      (t.f.j = (d, n) => {
+        var c = t.o(e, d) ? e[d] : void 0;
+        if (0 !== c)
+          if (c) n.push(c[2]);
+          else if (3666 != d) {
+            var r = new Promise((b, s) => (c = e[d] = [b, s]));
+            n.push((c[2] = r));
+            var l = t.p + t.u(d),
+              o = new Error();
+            t.l(
+              l,
+              (b) => {
+                if (t.o(e, d) && (0 !== (c = e[d]) && (e[d] = void 0), c)) {
+                  var s = b && ("load" === b.type ? "missing" : b.type),
+                    u = b && b.target && b.target.src;
+                  (o.message =
+                    "Loading chunk " + d + " failed.\n(" + s + ": " + u + ")"),
+                    (o.name = "ChunkLoadError"),
+                    (o.type = s),
+                    (o.request = u),
+                    c[1](o);
+                }
+              },
+              "chunk-" + d,
+              d
+            );
+          } else e[d] = 0;
+      }),
+        (t.O.j = (d) => 0 === e[d]);
+      var f = (d, n) => {
+          var o,
+            i,
+            [c, r, l] = n,
+            b = 0;
+          if (c.some((u) => 0 !== e[u])) {
+            for (o in r) t.o(r, o) && (t.m[o] = r[o]);
+            if (l) var s = l(t);
+          }
+          for (d && d(n); b < c.length; b++)
+            t.o(e, (i = c[b])) && e[i] && e[i][0](), (e[i] = 0);
+          return t.O(s);
+        },
+        a = (self.webpackChunkapp = self.webpackChunkapp || []);
+      a.forEach(f.bind(null, 0)), (a.push = f.bind(null, a.push.bind(a)));
+    })();
+})();
